1636d915dd46cbd41df467c1864b2864fc031e25
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-06-29 11:39:46 +0530

    updates

---
+++ src/schema/creators.schema.ts
@@ -0,0 +1,90 @@
+// src/clamio-coin/clamio-coin.entity.ts
+
+import { attribute, table } from '@aws/dynamodb-data-mapper-annotations';
+
+@table('ClamioCoin')
+export class ClamioCoin {
+  @attribute()
+  id: number;
+
+  @attribute()
+  user_id: number;
+
+  @attribute()
+  amount: number;
+
+  @attribute()
+  reason: string;
+}
+
+@table('Community')
+export class Community {
+  @attribute()
+  id: number;
+
+  @attribute()
+  creator_id: number;
+
+  @attribute()
+  name: string;
+
+  @attribute()
+  description: string;
+
+  @attribute()
+  thumbnail_url: string;
+}
+
+@table('Achievement')
+export class Achievement {
+  @attribute()
+  id: number;
+
+  @attribute()
+  name: string;
+
+  @attribute()
+  threshold: number;
+}
+
+enum ProductCategory {
+  TRAVEL_GUIDE = 'travel_guide',
+  ESPORTS_GUIDE = 'esports_guide',
+  FOOD_RECIPE = 'food_recipe',
+  TAROT_READING = 'tarot_reading',
+  DIET_PLAN = 'diet_plan',
+  DIGITAL_ART = 'digital_art',
+}
+
+@table('Product')
+export class Product {
+  @attribute()
+  id: number;
+
+  @attribute()
+  title: string;
+
+  @attribute()
+  description: string;
+
+  @attribute()
+  category: ProductCategory;
+
+  @attribute()
+  price: number;
+
+  @attribute()
+  thumbnail_url: string;
+
+  @attribute()
+  file_url: string;
+
+  @attribute()
+  content_type: string;
+
+  @attribute()
+  creator_id: number;
+
+  @attribute()
+  is_active: boolean;
+}
