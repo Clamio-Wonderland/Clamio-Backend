df69831ac8f3d0221e750d038a243c14774e0356
Nayanmoni <nayancoding@gmial.com>
2024-07-19 11:09:21 +0530

    configured creator community product

--- src/community/community.controller.ts
+++ src/community/community.controller.ts
@@ -8,30 +8,29 @@ import { JwtAuthGuard } from 'src/guards/JwtAuthGuard';
 export class CommunityController {
   constructor(private readonly communityService: CommunityService) {}
 
-  // Done
   @Post()
   @UseGuards(JwtAuthGuard)
   create(@Body() createCommunityDto: CreateCommunityDto) {
     return this.communityService.create(createCommunityDto);
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.communityService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.communityService.findAll();
+  }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.communityService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.communityService.findOne(+id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateCommunityDto: UpdateCommunityDto) {
-  //   return this.communityService.update(+id, updateCommunityDto);
-  // }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCommunityDto: UpdateCommunityDto) {
+    return this.communityService.update(+id, updateCommunityDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.communityService.remove(+id);
-  // }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.communityService.remove(+id);
+  }
 }
--- src/community/community.service.ts
+++ src/community/community.service.ts
@@ -39,19 +39,19 @@ export class CommunityService {
     
   }
 
-  // findAll() {
-  //   return `This action returns all community`;
-  // }
+  findAll() {
+    return `This action returns all community`;
+  }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} community`;
-  // }
+  findOne(id: number) {
+    return `This action returns a #${id} community`;
+  }
 
-  // update(id: number, updateCommunityDto: UpdateCommunityDto) {
-  //   return `This action updates a #${id} community`;
-  // }
+  update(id: number, updateCommunityDto: UpdateCommunityDto) {
+    return `This action updates a #${id} community`;
+  }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} community`;
-  // }
+  remove(id: number) {
+    return `This action removes a #${id} community`;
+  }
 }
