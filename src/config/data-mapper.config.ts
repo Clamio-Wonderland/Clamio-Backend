047207153427746a504067947d21240c4aac7b16
Anike567 <ka344057@gmail.com>
2024-07-23 21:59:41 +0530

    completed all basic product route and services

--- src/config/data-mapper.config.ts
+++ src/config/data-mapper.config.ts
@@ -1,10 +1,13 @@
 import { DataMapper } from '@aws/dynamodb-data-mapper';
 import * as AWS from 'aws-sdk';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: 'AKIAYDYCMARVWXT3UX5V',
-  secretAccessKey: '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',
+    region:process.env.AWS_REGION,
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const dynamoDBClientV2 = new AWS.DynamoDB();
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,26 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, UploadedFiles, FileValidator } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { JwtAuthGuard } from 'src/guards/JwtAuthGuard';
+import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { ParseFilePipe, FileTypeValidator } from '@nestjs/common';
+import { application } from 'express';
+
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
 
   @Post()
-  @UseGuards(JwtAuthGuard)
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  // @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'images', maxCount: 4 },
+    { name: 'product', maxCount: 1 }
+  ]))
+
+  async create(
+
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFiles() files : {productImages?: Express.Multer.File[], Product?:Express.Multer.File[]}
+  ) {
+      return this.productService.create(createProductDto,files)
   }
 
+
+
   @Get()
+  // @UseGuards(JwtAuthGuard)
   findAll() {
     return this.productService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  // @UseGuards(JwtAuthGuard)
+  async findOne(@Param('id') id: string) {
     return this.productService.findOne(id);
   }
 
@@ -30,6 +47,7 @@ export class ProductController {
   // }
 
   @Delete(':id')
+  // @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.productService.remove(id);
   }
