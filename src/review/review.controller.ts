7695c04fc96005ed6049d853e8149caaeefc287b
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-08-18 16:24:31 +0530

    get all reviews for a particular product return by pagination

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,34 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { PaginationDto } from './dto/pagination-dto';
+import { Product } from 'src/schema/product-schema';
+import { paginationToken } from 'aws-sdk/clients/supportapp';
+import { Review } from 'src/schema/review-schema';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewService.create(createReviewDto);
+  @Get('product/:productId')
+  async getReviewsByProductId(
+    @Param('productId') productId: string,
+    @Query() PaginationDto: PaginationDto,
+  ): Promise<Review[]> {
+    const { page = 1, limit = 10 } = PaginationDto;
+    return this.reviewService.getReviewsByProductId(productId, page, limit);
   }
 
-  @Get()
-  findAll() {
-    return this.reviewService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
-    return this.reviewService.update(+id, updateReviewDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reviewService.remove(+id);
+  @Get('product/user/:productId')
+  async getReviewOfProductByUser(
+    @Param('productId') ProductId: string,
+    @Body('userId') userId: string,
+  ) {
+    return this.reviewService.getReviewsByProductIdOfUser(ProductId, userId);
   }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,26 +1,84 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { Review } from 'src/schema/review-schema';
+import { dataMapper } from 'src/config/data-mapper.config';
+import { DataMapper, QueryOptions } from '@aws/dynamodb-data-mapper';
 
 @Injectable()
 export class ReviewService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  private readonly dataMapper: DataMapper;
+  constructor() {
+    this.dataMapper = dataMapper;
   }
 
-  findAll() {
-    return `This action returns all review`;
+  async getReviewsByProductId(
+    productId: string,
+    page: number = 1,
+    limit: number = 10,
+  ): Promise<Review[]> {
+    let lastkey;
+    let data: Review[] = [];
+    let skip = (page - 1) * limit;
+
+    try {
+      const paginator = this.dataMapper.query(
+        Review,
+        { productId },
+        { limit, startKey: lastkey },
+      );
+
+      for await (const review of paginator) {
+        if (skip > 0) {
+          skip--;
+          continue;
+        }
+        data.push(review);
+        if (data.length === limit) {
+          //@ts-ignore
+          lastkey = paginator.lastEvaluatedKey;
+          break;
+        }
+      }
+
+      if (data.length === 0)
+        throw new NotFoundException('Error fetching reviews.');
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException('Error fetching reviews.');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
-  }
+  async getReviewsByProductIdOfUser(productId: string, userId: string) {
+    const reviews: Review[] = [];
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
+    const query: QueryOptions = {
+      filter: {
+        type: 'Equals',
+        subject: 'userId',
+        object: userId,
+      },
+    };
 
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+    try {
+      for await (const review of this.dataMapper.query(
+        Review,
+        { productId },
+        query,
+      )) {
+        reviews.push(review);
+      }
+
+      if (reviews.length === 0) {
+        return {
+          message: `No reviews found for product Id ${productId} and by user Id ${userId}`,
+        };
+      }
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException('Error fetching reviews.');
+    }
   }
 }
