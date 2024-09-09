79a97dd2c76c13904b56676eefee0918e36d3ae9
Nayanmoni <nayancoding@gmial.com>
2024-09-09 23:13:06 +0530

    added configuration

--- src/config/data-mapper.config.ts
+++ src/config/data-mapper.config.ts
@@ -7,10 +7,6 @@ const AWS_REGION = process.env.AWS_REGION;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
-console.log(AWS_REGION);
-console.log(AWS_ACCESS_KEY_ID);
-console.log(AWS_SECRET_ACCESS_KEY);
-
 AWS.config.update({
   region: AWS_REGION,
   accessKeyId: AWS_ACCESS_KEY_ID,
