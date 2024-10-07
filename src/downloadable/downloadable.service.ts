c93a25eacad95fe6f842502ee9c72aa42ce5cc27
Anike567 <ka344057@gmail.com>
2024-10-07 12:27:47 +0530

    resulved minor issues

--- src/downloadable/downloadable.service.ts
+++ src/downloadable/downloadable.service.ts
@@ -7,6 +7,8 @@ import { Product } from 'src/schema/product-schema';
 import { createReadStream } from 'fs';
 import { OrderService } from 'src/order/order.service';
 import { BadRequestException } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+
 
 @Injectable()
 export class DownloadableService {
@@ -40,58 +42,51 @@ export class DownloadableService {
     return `This action removes a #${id} downloadable`;
   }
 
-
-
-
-
   async handleDownload(product_id: string, res, user_id: string) {
     try {
-
       const ordersByUser = await this.orderService.getUserOrders(user_id);
-
-      const product = await this.dataMapper.get(Object.assign(new Product, { _id: product_id }));
-
-
-
+      const product = await this.dataMapper.get(Object.assign(new Product(), { _id: product_id }));
+  
       if (!product) {
         throw new BadRequestException('Product not found');
       }
-
-
+  
       let isValidOrder = true;
-      // for (let i = 0; i < ordersByUser.length; i++) {
-
-      //   if (ordersByUser[i].items.find(item => item.product_id === product_id)) {
-      //     isValidOrder = true;
-      //     break;
-      //   }
-      // }
-
+  
       if (isValidOrder) {
+        const fileUrl = product.file_url[0];
+        const fileKey = fileUrl.split('/').pop(); 
+  
+        const s3 = new S3();
+  
+        const s3Params = {
+          Bucket: 'clamio-product-image', 
+          Key: fileKey,  
+        };
+  
+        const fileStream = s3.getObject(s3Params).createReadStream();
         
-        
-        const file_url = product.file_url[0];
-
-        const fileStream = await createReadStream(file_url);
         res.set({
           'Content-Type': 'application/octet-stream',
-          'Content-Disposition': `attachment; filename="${product.title}.${product.content_type}"`
-         
+          'Content-Disposition': `attachment; filename="${product.title}.${product.content_type}"`,
         });
-
+  
         fileStream.pipe(res);
+      } else {
+        return "Please order the product first.";
       }
-      else{
-        return "ordered product first";
-      }
-
-
-
+  
     } catch (error) {
       console.error('Error during file download:', error); // Log the error for debugging
       throw new BadRequestException('Error during file download');
     }
   }
+  
 
 
+
+  
+
+ 
+  
 }
